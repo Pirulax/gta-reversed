a4f3699e75e356853a80671a86fae3dbfd9cfcc6
Pirulax <patrikjankovics7@gmail.com>
2026-08-10 23:51:52 +0200

    fix(CTaskComplexHitByGunResponse): Fix incorrect implementation of `MakeAbotable`

--- source/game_sa/Tasks/TaskTypes/TaskComplexHitByGunResponse.h
+++ source/game_sa/Tasks/TaskTypes/TaskComplexHitByGunResponse.h
@@ -22,6 +22,7 @@ public:
 
     CTask*    Clone() const override { return new CTaskComplexHitByGunResponse{ *this }; }
     eTaskType GetTaskType() const override { return Type; }
+    bool      MakeAbortable(CPed* ped, eAbortPriority priority = ABORT_PRIORITY_URGENT, const CEvent* event = nullptr) override;
     CTask*    CreateNextSubTask(CPed* ped) override { return nullptr; }
     CTask*    CreateFirstSubTask(CPed* ped) override;
     CTask*    ControlSubTask(CPed* ped) override { return m_pSubTask; }
